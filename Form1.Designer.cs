b152dd67c9532d7b98fd54304cc5265934231bad
Johan <Johan@DESKTOP-RV4IECO>
2023-09-15 19:53:36 -0700

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Ingreso_a_pacientes
+{
+    partial class Forma1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Forma1));
+            this.txtusuario = new System.Windows.Forms.TextBox();
+            this.txtcontra = new System.Windows.Forms.TextBox();
+            this.cmdaceptar = new System.Windows.Forms.Button();
+            this.cmdsalir = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtusuario
+            // 
+            this.txtusuario.Location = new System.Drawing.Point(138, 131);
+            this.txtusuario.Name = "txtusuario";
+            this.txtusuario.Size = new System.Drawing.Size(100, 20);
+            this.txtusuario.TabIndex = 0;
+            // 
+            // txtcontra
+            // 
+            this.txtcontra.Location = new System.Drawing.Point(138, 222);
+            this.txtcontra.Name = "txtcontra";
+            this.txtcontra.PasswordChar = '*';
+            this.txtcontra.Size = new System.Drawing.Size(100, 20);
+            this.txtcontra.TabIndex = 1;
+            // 
+            // cmdaceptar
+            // 
+            this.cmdaceptar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdaceptar.Location = new System.Drawing.Point(36, 254);
+            this.cmdaceptar.Name = "cmdaceptar";
+            this.cmdaceptar.Size = new System.Drawing.Size(87, 41);
+            this.cmdaceptar.TabIndex = 2;
+            this.cmdaceptar.Text = "&Aceptar";
+            this.cmdaceptar.UseVisualStyleBackColor = true;
+            this.cmdaceptar.Click += new System.EventHandler(this.cmdaceptar_Click);
+            // 
+            // cmdsalir
+            // 
+            this.cmdsalir.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cmdsalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdsalir.Location = new System.Drawing.Point(245, 254);
+            this.cmdsalir.Name = "cmdsalir";
+            this.cmdsalir.Size = new System.Drawing.Size(89, 41);
+            this.cmdsalir.TabIndex = 3;
+            this.cmdsalir.Text = "&Salir";
+            this.cmdsalir.UseVisualStyleBackColor = true;
+            this.cmdsalir.Click += new System.EventHandler(this.cmdsalir_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(138, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 62);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(154, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "&Usuario";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(134, 185);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "&Contrtaseña";
+            // 
+            // Forma1
+            // 
+            this.AcceptButton = this.cmdaceptar;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.CancelButton = this.cmdsalir;
+            this.ClientSize = new System.Drawing.Size(383, 335);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmdsalir);
+            this.Controls.Add(this.cmdaceptar);
+            this.Controls.Add(this.txtcontra);
+            this.Controls.Add(this.txtusuario);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Forma1";
+            this.Text = "Ingreso a pacientes";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtusuario;
+        private System.Windows.Forms.TextBox txtcontra;
+        private System.Windows.Forms.Button cmdaceptar;
+        private System.Windows.Forms.Button cmdsalir;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ingreso_a_pacientes
+{
+    public partial class Forma1 : Form
+    {
+        public Forma1()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdaceptar_Click(object sender, EventArgs e)
+        {
+            if (txtusuario.Text == "JJHR" & txtcontra.Text == "0000")
+            {
+                Forma2 forma2 = new Forma2();
+                forma2.Show(this);
+                this.Visible = false;
+            }
+            else
+            {
+                MessageBox.Show("Datos incorrectos");
+                txtusuario.Clear();
+                txtcontra.Clear();
+                txtusuario.Focus();
+            }
+        }
+
+        private void cmdsalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
